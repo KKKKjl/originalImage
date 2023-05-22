a42b0dd76b832ac81926dceb306cbac5466d0381
KKKKjl <jingleilcx@gmail.com>
2023-05-22 21:35:38 +0800

    chore: Refactor Dockerfile CMD and deploy.sh docker run command

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM golang:1.20-alpine as builder
 
+# ENV GO111MODULE on
+# ENV GOOS linux
+# ENV CGO_ENABLED 0
+# ENV GOPROXY https://goproxy.cn,direct
+
 WORKDIR /build
 
 COPY . .
@@ -12,8 +17,4 @@ COPY --from=builder /build/app .
 
 EXPOSE 8080
 
-CMD [ "./app","-c", "/etc/config.json" ]
-
-
-
-
+CMD [ "./app","-c", "/etc/app/config.json" ]
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 project=original_image
-current_date=$(date +%y%m%d)
+current_date=$(date +%y.%m.%d)
 tag="v${current_date}"
 
-docker stop $project
-docker rm $project
-
 docker build -t $project:$tag .
-docker run --name $project -dp 8000:8000 $project:$tag
+docker stop $project && docker rm $project
+docker run --name $project -dp 8080:8080 -v $(pwd)/etc:/etc/app $project:$tag
